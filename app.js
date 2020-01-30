99647c91b676d43a64f38225b0909de0fc079f1a
Deepa Aswathaiah <deepa.x.r@gmail.com>
2020-01-29 21:38:13 -0500

    Refactoring- adding author name

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/**
+ * @author Deepa Aswathaiah
+ */
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var app = express();
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
